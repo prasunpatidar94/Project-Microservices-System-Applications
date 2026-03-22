e3cc89a2984f3bdef8d9cbe03fec5ef203efbf21
Prasun_Patidar <52647586+prasunpatidar94@users.noreply.github.com>
2026-03-22 23:50:36 +0530

    first commit Transection

---
+++ Banking-Application/transaction-service-app/src/main/java/com/pp/sun/TransactionServiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.pp.sun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionServiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionServiceAppApplication.class, args);
+	}
+
+}
